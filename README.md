d33785cfc6c6cfe4808b82b37a4de33e8cffd1a0
Mateusz Gmerek <tojagmerek@gmail.com>
2023-07-05 13:38:22 +0200

    test

--- README.md
+++
@@ -1,1 +0,0 @@
-# nerdziki
---
+++ test/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const plikObrazu = 'test.jpg';
+  
+  // Wywołaj funkcję odczytującą napisy po załadowaniu strony
+  odczytajNapisy(plikObrazu);
+});
+
+async function odczytajNapisy(plikObrazu) {
+  const cv = await cvReady();
+
+  // Wczytaj obraz
+  const image = await cv.imreadAsync(plikObrazu);
+
+  // Konwertuj obraz na odcienie szarości
+  const gray = image.cvtColor(cv.COLOR_BGR2GRAY);
+
+  // Wykorzystaj bibliotekę Tesseract.js do odczytu napisów
+  const { createWorker } = Tesseract;
+  const worker = createWorker();
+
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+  const { data: { text } } = await worker.recognize(gray.getBuffer());
+  await worker.terminate();
+
+  // Wyświetl odczytany tekst na stronie
+  const outputElement = document.getElementById('output');
+  outputElement.textContent = text;
+}
+
+function cvReady() {
+  return new Promise((resolve, reject) => {
+    cv.onRuntimeInitialized = () => resolve(cv);
+  });
+}
