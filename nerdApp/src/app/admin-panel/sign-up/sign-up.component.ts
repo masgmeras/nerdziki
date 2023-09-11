e40b2a292a9c545ea79dbd97a1788ae604732258
Michal Gmerek <mgmerek@lot.pl>
2023-09-12 00:05:55 +0200

    Admin module

--- nerdApp/src/app/admin-panel/sign-up/sign-up.component.ts
+++ nerdApp/src/app/admin-panel/sign-up/sign-up.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import {FormBuilder} from "@angular/forms";
+import {Router} from "@angular/router";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
+import {AdminService} from "../admin.service";
+import {from} from "rxjs";
 
 @Component({
   selector: 'app-sign-up',
@@ -7,4 +12,33 @@ import { Component } from '@angular/core';
 })
 export class SignUpComponent {
 
+  signUpForm = this.formBuilder.group({
+    email: '',
+    password: ''
+  });
+  errors: any;
+
+  constructor(private formBuilder: FormBuilder,
+              private router: Router,
+              private auth: AngularFireAuth,
+              private adminService: AdminService) {
+  }
+
+
+  onSubmit(): void {
+    console.warn('Your order has been submitted', this.signUpForm.value);
+    from(this.auth.createUserWithEmailAndPassword(<string>this.signUpForm.value.email, <string>this.signUpForm.value.password))
+        .subscribe({
+          next: (v) => {
+            console.log(v);
+            this.router.navigate(['main'])
+          },
+          error: (e) => {
+            console.error(e);
+            this.errors = e;
+          },
+          complete: () => console.info('complete')
+        })
+
+  }
 }
